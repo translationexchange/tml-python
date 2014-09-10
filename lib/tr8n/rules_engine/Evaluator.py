0a1e47236efaf5c71a22a52d7d13a33c0b9a9254
Randell <randell182@gmail.com>
2014-09-10 06:58:25 -0700

    more interim commit to evaluator

--- lib/tr8n/rules_engine/Evaluator.py
+++ lib/tr8n/rules_engine/Evaluator.py
@@ -29,11 +29,20 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #++
-
 __author__ = 'randell'
 
 
+from datetime import date
+import re
+
 class Evaluator:
+
+		@staticmethod
+		def drop():
+			def f(list,n):
+				[list.pop(0) for i in (0,n)]
+				return list
+			return f
 
 		def __init__(self):
 			self.vars = {}
@@ -42,67 +51,63 @@ class Evaluator:
 					self.vars[l] = r
 					return r
 				return f
-			def drop():
-				def f(list,n):
-					[list.pop(0) for i in (0,n)]
-					return list
-				return f
+
 
 			self.env = {
 			    # McCarthy's Elementary S-functions and Predicates
 			    'label'   : lambda l, r : label()(l,r),
 			    'quote'   : lambda expr :       expr ,
 			    'car'     : lambda list :       list[1],
-			    'cdr'     : lambda list:      drop()(list,1),
+			    'cdr'     : lambda list:      Evaluator.drop()(list,1),
 			    'cons'    : lambda e, cell:    [e] + cell ,
 			    'eq'      : lambda l, r:       l == r ,
 			    'atom'    : lambda expr:       isinstance(expr,(type(None),str,int,float,bool)),
 			    'cond'    : lambda c, t, f:    (self.evaluate(t) if self.evaluate(c) else self.evaluate(f))
 
 			    # Tr8n Extensions
-				'='       : lambda l, r :       l == r                                              # ['=', 1, 2]
-			#     '!='      : lambda { |l, r|      l != r },                                             # ['!=', 1, 2]
-			#     '<'       : lambda { |l, r|      l < r },                                              # ['<', 1, 2]
-			#     '>'       : lambda { |l, r|      l > r },                                              # ['>', 1, 2]
-			#     '+'       : lambda { |l, r|      l + r },                                              # ['+', 1, 2]
-			#     '-'       : lambda { |l, r|      l - r },                                              # ['-', 1, 2]
-			#     '*'       : lambda { |l, r|      l * r },                                              # ['*', 1, 2]
-			#     '%'       : lambda { |l, r|      l % r },                                              # ['%', 14, 10]
-			#     'mod'     : lambda { |l, r|      l % r },                                              # ['mod', '@n', 10]
-			#     '/'       : lambda { |l, r|      (l * 1.0) / r },                                      # ['/', 1, 2]
-			#     '!'       : lambda { |expr|      not expr },                                           # ['!', ['true']]
-			#     'not'     : lambda { |val|       not val },                                            # ['not', ['true']]
-			#     '&&'      : lambda { |*expr|     expr.all?{|e| evaluate(e)} },                         # ['&&', [], [], ...]
-			#     'and'     : lambda { |*expr|     expr.all?{|e| evaluate(e)} },                         # ['and', [], [], ...]
-			#     '||'      : lambda { |*expr|     expr.any?{|e| evaluate(e)} },                         # ['||', [], [], ...]
-			#     'or'      : lambda { |*expr|     expr.any?{|e| evaluate(e)} },                         # ['or', [], [], ...]
-			#     'if'      : lambda { |c, t, f|   evaluate(c) ? evaluate(t) : evaluate(f) },            # ['if', 'cond', 'true', 'false']
-			#     'let'     : lambda { |l, r|      @vars[l] = r },                                       # ['let', 'n', 5]
-			#     'true'    : lambda { true },                                                           # ['true']
-			#     'false'   : lambda { false },                                                          # ['false']
-			#
-			#     'date'    : lambda { |date|      Date.strptime(date, '%Y-%m-%d') },                    # ['date', '2010-01-01']
-			#     'today'   : lambda { Time.now.to_date },                                               # ['today']
-			#     'time'    : lambda { |expr|      Time.strptime(expr, '%Y-%m-%d %H:%M:%S') },           # ['time', '2010-01-01 10:10:05']
-			#     'now'     : lambda { Time.now },                                                       # ['now']
-			#
-			#     'append'  : lambda { |l, r|      r.to_s + l.to_s },                                    # ['append', 'world', 'hello ']
-			#     'prepend' : lambda { |l, r|      l.to_s + r.to_s },                                    # ['prepend', 'hello  ', 'world']
-			#     'match'   : lambda { |search, subject|                                                 # ['match', /a/, 'abc']
-			#       search = regexp_from_string(search)
-			#       not search.match(subject).nil?
-			#     },
-			#     'in'      : lambda { |values, search|                                                  # ['in', '1,2,3,5..10,20..24', '@n']
-			#       search = search.to_s.strip
-			#       values.split(',').each do |e|
-			#         if e.index('..')
-			#           bounds = e.strip.split('..')
-			#           return true if (bounds.first.strip..bounds.last.strip).include?(search)
-			#         end
-			#         return true if e.strip == search
-			#       end
-			#       false
-			#     },
+				'='       : lambda l, r :       l == r,                                              # ['=', 1, 2]
+			    '!='      : lambda l, r:      l != r ,                                             # ['!=', 1, 2]
+			    '<'       : lambda l, r:      l < r ,                                              # ['<', 1, 2]
+			    '>'       : lambda l, r:      l > r ,                                              # ['>', 1, 2]
+			    '+'       : lambda l, r:      l + r ,                                              # ['+', 1, 2]
+			    '-'       : lambda l, r:      l - r ,                                              # ['-', 1, 2]
+			    '*'       : lambda l, r:      l * r ,                                              # ['*', 1, 2]
+			    '%'       : lambda l, r:      l % r ,                                              # ['%', 14, 10]
+			    'mod'     : lambda l, r:      l % r ,                                              # ['mod', '@n', 10]
+			    '/'       : lambda l, r:      (l * 1.0) / r ,                                      # ['/', 1, 2]
+			    '!'       : lambda expr:      not expr ,                                           # ['!', ['true']]
+			    'not'     : lambda val:       not val ,                                            # ['not', ['true']]
+			    '&&'      : lambda exprs:     all(self.evaluate(expr) for expr in exprs),                         # ['&&', [], [], ...]
+			    'and'     : lambda exprs:     all(self.evaluate(expr) for expr in exprs),                         # ['and', [], [], ...]
+			    '::'      : lambda exprs:     any(self.evaluate(expr) for expr in exprs),                          # ['::', [], [], ...]
+			    'or'      : lambda exprs:     any(self.evaluate(expr) for expr in  exprs),                          # ['or', [], [], ...]
+			    'if'      : lambda c, t, f:   self.evaluate(t) if self.evaluate(c) else  self.evaluate(f) ,            # ['if', 'cond', 'true', 'false']
+			    'let'     : lambda l, r:      label()(l,r) ,                                       # ['let', 'n', 5]
+			    'true'    : lambda  True ,                                                           # ['true']
+			    'false'   : lambda  False ,                                                          # ['false']
+			
+			    'date'    : lambda date:      date.strptime(date, '%Y-%m-%d'),                    # ['date', '2010-01-01']
+			    'today'   : lambda date.now().today(),                                               # ['today']
+			    'time'    : lambda time:      date.strptime(date '%Y-%m-%d %H:%M:%S'),           # ['time', '2010-01-01 10:10:05']
+			    'now'     : lambda date.now(),                                                       # ['now']
+			
+			    'append'  : lambda l, r:      str(r) + str(l) ,                                    # ['append', 'world', 'hello ']
+			    'prepend' : lambda l, r:      str(l) + str(r)                                     # ['prepend', 'hello  ', 'world']
+			    # 'match'   : lambda { |search, subject|                                                 # ['match', /a/, 'abc']
+			    #   search = regexp_from_string(search)
+			    #   not search.match(subject).nil?
+			    # },
+			    # 'in'      : lambda { |values, search|                                                  # ['in', '1,2,3,5..10,20..24', '@n']
+			    #   search = search.to_s.strip
+			    #   values.split(',').each do |e|
+			    #     if e.index('..')
+			    #       bounds = e.strip.split('..')
+			    #       return true if (bounds.first.strip..bounds.last.strip).include?(search)
+			    #     end
+			    #     return true if e.strip == search
+			    #   end
+			    #   false
+			    # },
 			#     'within'  : lambda { |values, search|                                                 # ['within', '0..3', '@n']
 			#       bounds = values.split('..').map{|d| Integer(d)}
 			#       (bounds.first..bounds.last).include?(search)
@@ -128,8 +133,11 @@ class Evaluator:
 			#     },
 			}
 
-    #   def regexp_from_string(str)
-    #     return Regexp.new(/#{str}/) unless /^\//.match(str)
+      def regexp_from_string(self,str)
+            pattern = re.compile(str)
+            pattern2 = "^\/"
+            match1 = re.match(pattern2,str)
+            return pattern if not match1 else None
     #
     #     str = str.gsub(/^\//, '')
     #
@@ -146,23 +154,22 @@ class Evaluator:
     #     @vars = {}
     #   end
     #
-    #   def apply(fn, args)
-    #     raise "undefined symbols #{fn}" unless @env.keys.include?(fn)
-    #     @env[fn].call(*args)
-    #   end
+		def apply(self, fn, args)
+			#raise "undefined symbols #{fn}" unless @env.keys.include?(fn)
+			self.env[fn].call(*args)
+
     #
-		def evaluate(expr)
-			# if @env['atom'].call(expr)
-			#   return @vars[expr] if expr.is_a?(String) and @vars[expr]
-			#   return expr
-			# end
-			#
-			# fn = expr[0]
-			# args = expr.drop(1)
-			#
-			# unless ['quote', 'car', 'cdr', 'cond', 'if', '&&', '||', 'and', 'or', 'true', 'false', 'let', 'count', 'all', 'any'].member?(fn)
-			#   args = args.map { |a| self.evaluate(a) }
-			# end
-			# apply(fn, args)
-		end
+		def evaluate(self,expr):
+			None
+			if self.env['atom'].call(expr):
+			  return self.vars[expr] if (expr is string and self.vars[expr]) else expr
+
+			fn = expr[0]
+			args = Evaluator.drop(expr,1)
+
+			unless ['quote', 'car', 'cdr', 'cond', 'if', '&&', '||', 'and', 'or', 'true', 'false', 'let', 'count', 'all', 'any'].member?(fn)
+			  args = args.map { |a| self.evaluate(a) }
+			end
+			self.apply(fn, args)
+
     # end

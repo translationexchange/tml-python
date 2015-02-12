5831c256dcd67efa0253560ce7a30a899010853a
Randell <randell182@gmail.com>
2015-02-12 09:28:45 -0800

    Start work again on python lib language SDK

--- tr8n/rules_engine.py
+++ tr8n/rules_engine.py
@@ -79,8 +79,8 @@ class Evaluator:
 				return f
 			def in_f():
 				def f(values,search):
-				    search = str(search).strip()
-				    for e in values.split(','):
+					search = str(search).strip()
+					for e in values.split(','):
 						if e.find('..') > 0:
 							bounds = e.strip().split('..')
 							if search in list(range(bounds[0].strip(),bounds[-1].strip())):
@@ -93,7 +93,7 @@ class Evaluator:
 				def f(search,replace_with, s):
 					if re.compile('/\/i$/').match(subject):
 						replace_with = re.compile("/\$(\d+)/").sub(replace_with,'\\\\\1')						 # for compatibility with Perl notation
-			        search = self.regexp_from_string(search)
+					search = self.regexp_from_string(search)
 					search.sub(subject,replace)
 					return subject
 				return f
@@ -103,66 +103,66 @@ class Evaluator:
 					return search in list(range(bounds[0],bounds[-1]))
 
 			self.env = {
-			    # McCarthy's Elementary S-functions and Predicates
-			    'label'   : lambda l, r : label()(l,r),
-			    'quote'   : lambda expr :       expr ,
-			    'car'     : lambda list :       list[1],
-			    'cdr'     : lambda list:      Evaluator.drop()(list,1),
-			    'cons'    : lambda e, cell:    [e] + cell ,
-			    'eq'      : lambda l, r:       l == r ,
-			    'atom'    : lambda expr:       isinstance(expr,(type(None),str,int,float,bool)),
-			    'cond'    : lambda c, t, f:    (self.evaluate(t) if self.evaluate(c) else self.evaluate(f))
+				# McCarthy's Elementary S-functions and Predicates
+				'label'   : lambda l, r : label()(l,r),
+				'quote'   : lambda expr :       expr ,
+				'car'     : lambda list :       list[1],
+				'cdr'     : lambda list:      Evaluator.drop()(list,1),
+				'cons'    : lambda e, cell:    [e] + cell ,
+				'eq'      : lambda l, r:       l == r ,
+				'atom'    : lambda expr:       isinstance(expr,(type(None),str,int,float,bool)),
+				'cond'    : lambda c, t, f:    (self.evaluate(t) if self.evaluate(c) else self.evaluate(f))
 
-			    # Tr8n Extensions
+				# Tr8n Extensions
 				'='       : lambda l, r :       l == r,                                              # ['=', 1, 2]
-			    '!='      : lambda l, r:      l != r ,                                             # ['!=', 1, 2]
-			    '<'       : lambda l, r:      l < r ,                                              # ['<', 1, 2]
-			    '>'       : lambda l, r:      l > r ,                                              # ['>', 1, 2]
-			    '+'       : lambda l, r:      l + r ,                                              # ['+', 1, 2]
-			    '-'       : lambda l, r:      l - r ,                                              # ['-', 1, 2]
-			    '*'       : lambda l, r:      l * r ,                                              # ['*', 1, 2]
-			    '%'       : lambda l, r:      l % r ,                                              # ['%', 14, 10]
-			    'mod'     : lambda l, r:      l % r ,                                              # ['mod', '@n', 10]
-			    '/'       : lambda l, r:      (l * 1.0) / r ,                                      # ['/', 1, 2]
-			    '!'       : lambda expr:      not expr ,                                           # ['!', ['true']]
-			    'not'     : lambda val:       not val ,                                            # ['not', ['true']]
-			    '&&'      : lambda exprs:     all(self.evaluate(expr) for expr in exprs),                         # ['&&', [], [], ...]
-			    'and'     : lambda exprs:     all(self.evaluate(expr) for expr in exprs),                         # ['and', [], [], ...]
-			    '::'      : lambda exprs:     any(self.evaluate(expr) for expr in exprs),                          # ['::', [], [], ...]
-			    'or'      : lambda exprs:     any(self.evaluate(expr) for expr in  exprs),                          # ['or', [], [], ...]
-			    'if'      : lambda c, t, f:   self.evaluate(t) if self.evaluate(c) else  self.evaluate(f) ,            # ['if', 'cond', 'true', 'false']
-			    'let'     : lambda l, r:      label()(l,r) ,                                       # ['let', 'n', 5]
-			    'true'    : lambda  True ,                                                           # ['true']
-			    'false'   : lambda  False ,                                                          # ['false']
+				'!='      : lambda l, r:      l != r ,                                             # ['!=', 1, 2]
+				'<'       : lambda l, r:      l < r ,                                              # ['<', 1, 2]
+				'>'       : lambda l, r:      l > r ,                                              # ['>', 1, 2]
+				'+'       : lambda l, r:      l + r ,                                              # ['+', 1, 2]
+				'-'       : lambda l, r:      l - r ,                                              # ['-', 1, 2]
+				'*'       : lambda l, r:      l * r ,                                              # ['*', 1, 2]
+				'%'       : lambda l, r:      l % r ,                                              # ['%', 14, 10]
+				'mod'     : lambda l, r:      l % r ,                                              # ['mod', '@n', 10]
+				'/'       : lambda l, r:      (l * 1.0) / r ,                                      # ['/', 1, 2]
+				'!'       : lambda expr:      not expr ,                                           # ['!', ['true']]
+				'not'     : lambda val:       not val ,                                            # ['not', ['true']]
+				'&&'      : lambda exprs:     all(self.evaluate(expr) for expr in exprs),                         # ['&&', [], [], ...]
+				'and'     : lambda exprs:     all(self.evaluate(expr) for expr in exprs),                         # ['and', [], [], ...]
+				'::'      : lambda exprs:     any(self.evaluate(expr) for expr in exprs),                          # ['::', [], [], ...]
+				'or'      : lambda exprs:     any(self.evaluate(expr) for expr in  exprs),                          # ['or', [], [], ...]
+				'if'      : lambda c, t, f:   self.evaluate(t) if self.evaluate(c) else  self.evaluate(f) ,            # ['if', 'cond', 'true', 'false']
+				'let'     : lambda l, r:      label()(l,r) ,                                       # ['let', 'n', 5]
+				'true'    : lambda  True ,                                                           # ['true']
+				'false'   : lambda  False ,                                                          # ['false']
 
-			    'date'    : lambda date:      date.strptime(date, '%Y-%m-%d'),                    # ['date', '2010-01-01']
-			    'today'   : lambda date.now().today(),                                               # ['today']
-			    'time'    : lambda time:      date.strptime(date '%Y-%m-%d %H:%M:%S'),           # ['time', '2010-01-01 10:10:05']
-			    'now'     : lambda date.now(),                                                       # ['now']
+				'date'    : lambda date:      date.strptime(date, '%Y-%m-%d'),                    # ['date', '2010-01-01']
+				'today'   : lambda date.now().today(),                                               # ['today']
+				'time'    : lambda time:      date.strptime(date '%Y-%m-%d %H:%M:%S'),           # ['time', '2010-01-01 10:10:05']
+				'now'     : lambda date.now(),                                                       # ['now']
 
-			    'append'  : lambda l, r:      str(r) + str(l) ,                                    # ['append', 'world', 'hello ']
-			    'prepend' : lambda l, r:      str(l) + str(r),                                     # ['prepend', 'hello  ', 'world']
-			    'match'   : lambda search, subject:  not self.regexp_from_string(search).match(subject),   # ['match', /a/, 'abc']
-			    'in'      : lambda values, search: in_f()(values,search) ,                                                 # ['in', '1,2,3,5..10,20..24', '@n']
-			    'within'  : lambda values, search: within(values,search),                                                 # ['within', '0..3', '@n']
-			    'replace' : lambda search, replace, subject : replace()(search,replace,subject),
-			    'count'   : lambda list : len(self.vars[list] if isinstance(list,str) else list) , # ['count', '@genders']
-			    'all'     : lambda list, value: all([v == value for v in flatten(list)]),   # ['all', '@genders', 'male']
-			    'any'     : lambda list, value: any([v == value for v in flatten(list)]) # ['any', '@genders', 'female']
+				'append'  : lambda l, r:      str(r) + str(l) ,                                    # ['append', 'world', 'hello ']
+				'prepend' : lambda l, r:      str(l) + str(r),                                     # ['prepend', 'hello  ', 'world']
+				'match'   : lambda search, subject:  not self.regexp_from_string(search).match(subject),   # ['match', /a/, 'abc']
+				'in'      : lambda values, search: in_f()(values,search) ,                                                 # ['in', '1,2,3,5..10,20..24', '@n']
+				'within'  : lambda values, search: within(values,search),                                                 # ['within', '0..3', '@n']
+				'replace' : lambda search, replace, subject : replace()(search,replace,subject),
+				'count'   : lambda list : len(self.vars[list] if isinstance(list,str) else list) , # ['count', '@genders']
+				'all'     : lambda list, value: all([v == value for v in flatten(list)]),   # ['all', '@genders', 'male']
+				'any'     : lambda list, value: any([v == value for v in flatten(list)]) # ['any', '@genders', 'female']
 			}
 
 		def regexp_from_string(self,str):
-		    pattern = re.compile(str)
-		    pattern2 = "^\/"
-		    match1 = re.match(pattern2,str)
-		    if not match1:
-		        return pattern
-		    str = re.compile("/^\//").sub('',str)
-		    if re.compile("\/i$/").match(str):
+			pattern = re.compile(str)
+			pattern2 = "^\/"
+			match1 = re.match(pattern2,str)
+			if not match1:
+				return pattern
+			str = re.compile("/^\//").sub('',str)
+			if re.compile("\/i$/").match(str):
 				str = re.compile("/\/i$/").sub("",str)
 				return re.compile("/%s/i"%str)
-		    str = re.compile("/\/$/").sub('',str)
-		    return re.compile("/%s/"%str)
+			str = re.compile("/\/$/").sub('',str)
+			return re.compile("/%s/"%str)
 
 
 		def reset(self):
@@ -172,7 +172,7 @@ class Evaluator:
 			if (fn not in self.env.keys()):
 				raise ArgumentError("undefined symbols #{fn}")
 			self.env[fn].call(*args)
-    #
+	#
 		def evaluate(self,expr):
 			None
 			if self.env['atom'].call(expr):
@@ -186,5 +186,5 @@ class Evaluator:
 
 			self.apply(fn, args)
 
-    # end
+	# end
 

05eecec90c6857d05eb282d874b695a6fd4541b6
Andrey Toukmanov <a@toukmanov.ru>
2015-02-20 19:20:57 +0800

    Rules parser

--- tests/unit/rules_engine.py
+++ tests/unit/rules_engine.py
@@ -4,5 +4,63 @@ from tml.rules_engine import *
 
 
 class rules_engine(unittest.TestCase):
+    """ Test rules engine """
     def test_parser(self):
-        pass
+        """ Check is parser works correct """
+        self.assertEquals(
+                          ['mod', '10', '5'],
+                          parse('(mod 10 5)'),
+                          'Test parse')
+        self.assertEquals(['mod',['+', '2', 'PI'], '5'],
+                          parse('(mod (+ 2 PI) 5)'),
+                          'Parse incapsulated')
+        self.assertEquals(['mod','5', ['+', '2', 'PI']],
+                          parse('(mod 5 (+ 2 PI))'),
+                          'Parse incapsulated end')
+        self.assertEquals(['mod','5', ['+', '2', ['div', '9', '3']]],
+                          parse('(mod 5 (+ 2 (div 9 3)))'),
+                          '3 level')
+        self.assertEquals([['userfunction', 'name'],'a','b'],
+                          parse('((userfunction name) a b)'),
+                          'Userfunction support')
+        self.assertEquals(['upper','Quoted text with (braces)'],
+                          parse('(upper "Quoted text with (braces)")'),
+                          'Parse quoted')
+        self.assertEquals(['hello','world'],
+                  parse('(hello   world)'),
+                  'Ignore multispace')
+        # Check parse errrors:
+        with self.assertRaises(ParseError) as context:
+           parse('(hello')
+        self.assertEquals(ParseError.INVALID_SYNTAX, context.exception.code, 'No open brace')
+        self.assertEquals(5, context.exception.pos, 'Close bracket')
+
+        with self.assertRaises(ParseError) as context:
+           parse('hello')
+        self.assertEquals(ParseError.INVALID_SYNTAX, context.exception.code, 'No close brace')
+        self.assertEquals(0, context.exception.pos, 'Open bracket')
+
+        with self.assertRaises(ParseError) as context:
+           parse('(no"quote")')
+        self.assertEquals(ParseError.INVALID_SYNTAX, context.exception.code, 'Unexpected quote')
+        self.assertEquals(3, context.exception.pos, 'Unexpected quote pos')
+
+        with self.assertRaises(ParseError) as context:
+           parse('(quote "not closed)')
+        self.assertEquals(ParseError.QUOTE_IS_NOT_CLOSED, context.exception.code, 'Quote not closed')
+        self.assertEquals(7, context.exception.pos, 'Quote is not closed pos')
+
+        with self.assertRaises(ParseError) as context:
+           parse('(no ) brace)')
+        self.assertEquals(ParseError.UNEXPECTED_EXPRESSION_FINISH, context.exception.code, 'Unexpected brace')
+        self.assertEquals(4, context.exception.pos, 'Unexpected brace pos')
+
+        with self.assertRaises(ParseError) as context:
+           parse('(unexpected (close (brace inside))) deep)')
+        self.assertEquals(ParseError.UNEXPECTED_EXPRESSION_FINISH, context.exception.code, 'Unexpected brace')
+
+        with self.assertRaises(ParseError) as context:
+           parse('(not (closed (expression))')
+        self.assertEquals(ParseError.QUOTE_IS_NOT_CLOSED, context.exception.code, 'Not closed quote')
+        self.assertEquals(5, context.exception.pos, 'Not closed quote pos')
+
--- tml/rules_engine.py
+++ tml/rules_engine.py
@@ -37,27 +37,120 @@ from datetime import date
 from compiler.misc import flatten
 import re
 
+def parse(text):
+    """ Parse rule 
+        Args:
+            rule (string): string
+        Returns:
+            list: list of notations 
+    """
+    ret = []
+    pos = 0 # current symbol position
+    expression = [] # current expression
+    argument = None # current argument
+    quote = None
+    stack = [] # expressions stack
+    stack_pos = []
+    # Check that expression is in brackets:
+    if text[0] != '(':
+        raise ParseError('No opened bracked found', ParseError.INVALID_SYNTAX, text, 0)
+    if text[-1] != ')':
+        raise ParseError('No closed bracked found', ParseError.INVALID_SYNTAX, text, len(text)-1)
 
-class Parser:
-    def __init__(self, expression):
-        self.expression = expression
-        if re.compile('^\(').match(expression):
-            self.tokens = re.findall('/[()]|\w+|@\w+|[\+\-\!\|\=>&<\*\/%]+|".*?"|\'.*?\'/', expression)
+    def flush(expression, argument):
+        """ Flush argument to expression
+            Args:
+                expression (list): expression
+                argument (list|None): appended argument
+            Returns:
+                tuple(expression, argument)
+        """
+        if argument:
+            expression.append(''.join(argument))
+        return (expression, None)
 
-    def parse(self):
-        if not self.tokens: return self.expression
-        token = self.tokens.pop(0)
-        if not token: return None
-        if token == '(': return self.parse_list()
-        if re.compile("^['\"].*").match(token): return token[1:len(token) - 2]
-        if re.compile("/\d+/").match(token): return int(token)
-        return token
+    for let in text[1:-1]:
+        pos = pos + 1 # calculate symbol
+        if let == '"':
+            # Quotation like ("Some quoted text")
+            if quote is None:
+                # Start new argument:
+                # (upper ^"Any data inside")
+                if not argument is None:
+                    # Unexpected quotation inside argument: (print like^"to mix quotes")
+                    raise ParseError('Unexpected quote', ParseError.INVALID_SYNTAX, text, pos)
+                argument = []
+                quote = pos
+            else:
+                # Close argument:
+                # (upper "Any data inside^")
+                expression, argument = flush(expression, argument)
+                quote = None
+        elif not quote is None:
+            # We are inside quoted argument:
+            # ignore any special symbol and just push to argument
+            # like (upper "Any^ data inside")
+            argument.append(let)
+        else:
+            if let == '(':
+                # begin next expression: (sum ^(mod 25 10) 2):
+                expression, argument = flush(expression, argument)
+                stack.append(expression) # store current expression in stack
+                stack_pos.append(pos)
+                expression = [] # create empty expression
+            elif let == ')':
+                # end of embed expression: (sum (mod 25 10^) 2):
+                if len(stack) == 0:
+                    # No expression to pop:
+                    raise ParseError('Unexpected )', ParseError.UNEXPECTED_EXPRESSION_FINISH, text, pos)
+                inner_expression, argument = flush(expression, argument)
+                expression = stack.pop() # pop expression from stack
+                stack_pos.pop() # forgot about last pos
+                expression.append(inner_expression)
+            elif let == ' ':
+                # Next argument:
+                if not argument is None: # check argument for double space bug: (arg1  arg2)
+                    expression, argument = flush(expression, argument)
+            elif argument:
+                # Append data to argument:
+                argument.append(let)
+            else:
+                # Begin new argument:
+                argument = [let]
+    # OK, finished:
+    if quote:
+        # Quotation is not closed:
+        raise ParseError('Quotation is not closed', ParseError.QUOTE_IS_NOT_CLOSED, text, quote)
+    if len(stack_pos):
+        raise ParseError('Expression is not closed', ParseError.EXPRESSION_IS_NOT_CLOSED, text, stack_pos.pop())
+    # Flush last argument:
+    expression, argument = flush(expression, argument)
+    if len(expression) == 0:
+        raise ParseError('Expression is empty', ParseError.EMPTY_EXPRESSION, text, 0)
+    return expression
 
 
-    def parse_list(self):
-        list = [token for token in iter(self.parse(), ')')]
-        self.tokens.pop(0)
-        return list
+class ParseError(Exception):
+    """ Parse error """
+    INVALID_SYNTAX = 1
+    UNEXPECTED_EXPRESSION_FINISH = 2
+    QUOTE_IS_NOT_CLOSED = 3
+    EXPRESSION_IS_NOT_CLOSED = 4
+    EMPTY_EXPRESSION = 5
+    def __init__(self, message, code, text, pos = 0):
+        """ Expression parsion fault:
+            message (string): error message
+            code (int): error code
+            text (string): parsed expression text
+            pos (pos): position in text where error occurs
+        """
+        self.message = message
+        self.code = code
+        self.text = text
+        self.pos = pos
+
+    def __str__(self, *args, **kwargs):
+        return 'Expression parse error %s in "%s" at symbol %d' % (self.message, self.text, self.pos)
 
 
 class Evaluator:

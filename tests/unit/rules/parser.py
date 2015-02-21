7c38e17f5c71f79aae646b953b52086d4c1e9c02
Andrey Toukmanov <a@toukmanov.ru>
2015-02-21 13:11:34 +0800

    Extract parser to rules.parser

---
+++ tests/unit/rules/parser.py
@@ -0,0 +1,73 @@
+# encoding: UTF-8
+""" Test rules built-in functions """
+import unittest
+from tml.rules.parser import *
+
+
+class rules_parser(unittest.TestCase):
+    """ Test rules engine """
+    def test_parser(self):
+        """ Check is parser works correct """
+        self.assertEquals(
+                          ['mod', '10', '5'],
+                          parse('(mod 10 5)'),
+                          'Test parse')
+        self.assertEquals(['mod',['+', '2', 'PI'], '5'],
+                          parse('(mod (+ 2 PI) 5)'),
+                          'Parse incapsulated')
+        self.assertEquals(['mod','5', ['+', '2', 'PI']],
+                          parse('(mod 5 (+ 2 PI))'),
+                          'Parse incapsulated end')
+        self.assertEquals(['mod','5', ['+', '2', ['div', '9', '3']]],
+                          parse('(mod 5 (+ 2 (div 9 3)))'),
+                          '3 level')
+        self.assertEquals([['userfunction', 'name'],'a','b'],
+                          parse('((userfunction name) a b)'),
+                          'Userfunction support')
+        self.assertEquals(['upper','Quoted text with (braces)'],
+                          parse('(upper "Quoted text with (braces)")'),
+                          'Parse quoted')
+        self.assertEquals(['hello','world'],
+                  parse('(hello   world)'),
+                  'Ignore multispace')
+
+    def test_errors(self):
+        # Check parse errrors:
+        with self.assertRaises(ParseError) as context:
+           parse('(hello')
+        self.assertEquals(ParseError.INVALID_SYNTAX, context.exception.code, 'No open brace')
+        self.assertEquals(5, context.exception.pos, 'Close bracket')
+
+        with self.assertRaises(ParseError) as context:
+           parse('hello')
+        self.assertEquals(ParseError.INVALID_SYNTAX, context.exception.code, 'No close brace')
+        self.assertEquals(0, context.exception.pos, 'Open bracket')
+
+        with self.assertRaises(ParseError) as context:
+           parse('(no"quote")')
+        self.assertEquals(ParseError.INVALID_SYNTAX, context.exception.code, 'Unexpected quote')
+        self.assertEquals(3, context.exception.pos, 'Unexpected quote pos')
+
+        with self.assertRaises(ParseError) as context:
+           parse('(quote "not closed)')
+        self.assertEquals(ParseError.QUOTE_IS_NOT_CLOSED, context.exception.code, 'Quote not closed')
+        self.assertEquals(7, context.exception.pos, 'Quote is not closed pos')
+
+        with self.assertRaises(ParseError) as context:
+           parse('(no ) brace)')
+        self.assertEquals(ParseError.UNEXPECTED_EXPRESSION_FINISH, context.exception.code, 'Unexpected brace')
+        self.assertEquals(4, context.exception.pos, 'Unexpected brace pos')
+
+        with self.assertRaises(ParseError) as context:
+           parse('(unexpected (close (brace inside))) deep)')
+        self.assertEquals(ParseError.UNEXPECTED_EXPRESSION_FINISH, context.exception.code, 'Unexpected brace')
+
+        with self.assertRaises(ParseError) as context:
+           parse('(not (closed (expression))')
+        self.assertEquals(ParseError.EXPRESSION_IS_NOT_CLOSED, context.exception.code, 'Not closed quote')
+        self.assertEquals(5, context.exception.pos, 'Not closed quote pos')
+
+
+if __name__ == '__main__':
+    unittest.main()
+

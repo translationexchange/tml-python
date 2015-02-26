573561a2622891f1a353c603e1a3bb12b5ff5c1e
Andrey Toukmanov <a@toukmanov.ru>
2015-02-26 19:15:06 +0800

    Use Client mock in application unit test

--- tests/unit/application.py
+++ tests/unit/application.py
@@ -1,35 +1,34 @@
 # encoding: UTF-8
 import unittest
 from tml import Application, LanguageNotSupported
-
-class ClientMock(object):
-    def __init__(self, data = {}):
-        self.data = data
-
-    def get(self, url, params):
-        return self.data[url]
+from tests.mock import Client
 
 LANGUAGES = [{'locale':'ru'},{'locale':'en'}]
 
 class application(unittest.TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.client.read('applications/current')
+        self.client.read('applications/2')
+
+
     def test_instance(self):
-        app = Application.from_dict(ClientMock(), {'id':100, 'languages': LANGUAGES})
+        app = Application.from_dict(self.client, {'id':100, 'languages': LANGUAGES})
         self.assertEquals(100, app.id, 'id getter')
         self.assertEquals(LANGUAGES, app.languages, 'Check languages')
 
 
     def test_language(self):
-        app = Application(ClientMock(), 100, LANGUAGES)
+        app = Application(self.client, 100, LANGUAGES)
         self.assertEquals('languages/ru', app.get_language_url('ru'), 'URL for ru')
         self.assertEquals('languages/en', app.get_language_url('en'), 'URL for en')
         with self.assertRaises(LanguageNotSupported):
             app.get_language_url('de')
 
     def test_load(self):
-        c = ClientMock({'applications/10': {'id': 10, 'languages': LANGUAGES},
-                        'applications/current':{'id':20, 'languages': []}})
-        self.assertEquals(10, Application.load_by_id(c, 10).id, 'Load by id')
-        self.assertEquals(20, Application.load_default(c).id, 'Load default')
+        self.assertEquals(2, Application.load_by_id(self.client, 2).id, 'Load by id')
+        self.assertEquals(1, Application.load_default(self.client).id, 'Load default')
 
 
 if __name__ == '__main__':

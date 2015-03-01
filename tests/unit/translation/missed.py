e43a5d45ed2b10f14f68879850ca05b3c91c02c0
Andrey Toukmanov <a@toukmanov.ru>
2015-03-01 23:00:01 +0800

    Lazy submit

--- tests/unit/translation/missed.py
+++ tests/unit/translation/missed.py
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 from tests.mock import Client
 from tml.translation import Key
-from tml.translation.missed import MissedKey
+from tml.translation.missed import MissedKey, MissedKeysLazy
 import unittest
 from tml import Application
 from tml.language import Language
@@ -21,6 +21,19 @@ class missed_test(unittest.TestCase):
         self.assertEquals({'status':'Ok'}, m.append(Key(language = self.lang, level = 2, label = 'Hello', description = 'greeting')))
         self.assertEquals('post', self.c.method, 'post request')
         self.assertEquals({'source_keys':'[{"keys": [{"locale": "ru", "level": 2, "description": "greeting", "label": "Hello"}]}]'}, self.c.params, 'Missed keys')
+
+    def test_lazy_keys(self):
+        m = MissedKeysLazy(self.c)
+        self.c.url = None
+        m.append(Key(language = self.lang, level = 2, label = 'Hello', description = 'greeting'))
+        self.assertEquals(None, self.c.url, 'No requests after add')
+        m.append(Key(language = self.lang, level = 2, label = 'Hello again', description = 'greeting'))
+        m.submit_all()
+        self.assertEquals('sources/register_keys', self.c.url, 'Post')
+        self.assertEquals({'source_keys':'[{"keys": [{"locale": "ru", "level": 2, "description": "greeting", "label": "Hello"}, {"locale": "ru", "level": 2, "description": "greeting", "label": "Hello again"}]}]'}, self.c.params, 'Submit 2 keys')
+        self.c.url = None
+        m.submit_all()
+        self.assertEquals(None, self.c.url, 'No not submit twice')
 
 
 if __name__ == '__main__':
--- tml/translation/missed.py
+++ tml/translation/missed.py
@@ -21,3 +21,22 @@ class MissedKey(object):
         """
         return self.client.post('sources/register_keys',{'source_keys': dumps(missed_keys)})
 
+
+class MissedKeysLazy(MissedKey):
+    """ Submit all missed keys once """
+    keys = []
+    def append(self, key):
+        """ Add key to missed
+            Args:
+                key (Key): translation key
+        """
+        self.keys.append(key)
+
+    def submit_all(self):
+        if len(self.keys) == 0:
+            return
+        missed_keys = [{'keys':[key.as_dict for key in self.keys]}]
+        ret = self.submit(missed_keys)
+        self.keys = []
+        return ret
+

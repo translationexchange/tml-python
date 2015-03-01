cb1d74628c036860516963f41c2fb07b16ae9dd2
Andrey Toukmanov <a@toukmanov.ru>
2015-03-01 12:26:30 +0800

    Dict support in pagination

--- tests/unit/api/pagination.py
+++ tests/unit/api/pagination.py
@@ -15,6 +15,16 @@ class pagination(unittest.TestCase):
         self.assertEquals(1324560, full_list[0]['id'], 'Check order')
         self.assertEquals(1324559, full_list[2]['id'], 'Check order (append to end)')
 
+    def test_dict_pagination(self):
+        self.client.read('applications/1/translations', {'page':1, 'locale':'ru'}, 'applications/1/translations_1.json')
+        self.client.read('applications/1/translations', {'page':2, 'locale':'ru'}, 'applications/1/translations_2.json')
+        self.client.read('applications/1/translations', {'page':3, 'locale':'ru'}, 'applications/1/translations_3.json')
+        full_list = allpages(self.client, 'applications/1/translations',{'locale':'ru'})
+        self.assertEquals(5, len(full_list), 'Test paging')
+        self.assertEquals("{actor} любезно дала тебе {count||яблоко, яблока, яблок}", full_list['8ad5a7fe0a12729764e31a1e3ca80059'][0]['label'], 'Check first')
+        self.assertEquals("Тест", full_list['5174f88691edb354a9f46af6e7455bb8'][0]['label'], 'Check last')
+
+
     def test_no_pagination(self):
         self.client.read('translation_keys/8ad5a7fe0a12729764e31a1e3ca80059/translations', {'page':1}, 'translation_keys/8ad5a7fe0a12729764e31a1e3ca80059/translations.json')
         full_list = allpages(self.client, 'translation_keys/8ad5a7fe0a12729764e31a1e3ca80059/translations',{})
--- tml/api/pagination.py
+++ tml/api/pagination.py
@@ -1,5 +1,6 @@
 # encoding: UTF-8
 
+
 def allpages(client, url, params):
     """ Return results from all pages 
         Args:
@@ -9,10 +10,17 @@ def allpages(client, url, params):
     """
     total_pages = 1
     params['page'] = 0
-    ret = []
+    ret = None
     while params['page'] < total_pages:
         params['page'] = params['page'] + 1
         resp = client.get(url, params)
-        ret = ret + resp['results']
+        if ret is None:
+            ret = resp['results']
+        elif type(ret) is list:
+            ret = ret + resp['results']
+        elif type(ret) is dict:
+            ret.update(resp['results'])
+        else:
+            raise Exception('Results with type %s can not be used in pagination' % type(resp['results']))
         total_pages = resp['pagination']['total_pages']
     return ret

687350d11cf7584e931f9d231b597d5ba9b36b8b
Andrey Toukmanov <a@toukmanov.ru>
2015-03-01 16:23:23 +0800

    #fixes pass params in client.read

--- tests/unit/translation/__init__.py
+++ tests/unit/translation/__init__.py
@@ -15,8 +15,8 @@ class translation_test(unittest.TestCase):
     """ Test translations """
     def setUp(self):
         self.client = Client()
-        self.client.read('applications/current')
-        self.client.read('languages/ru')
+        self.client.read('applications/current', {'definition': 1})
+        self.client.read('languages/ru', {'definition': 1})
         self.app = Application.load_default(self.client)
         self.lang = Language.load_by_locale(self.app, 'ru')
 
@@ -25,7 +25,6 @@ class translation_test(unittest.TestCase):
         self.assertEquals('{name} give you {count} apples', k.label, 'label')
         self.assertEquals('apple', k.description, 'description')
         self.assertEquals(self.lang.locale, k.language.locale, 'language')
-
 
     def test_key_hash(self):
         """ Test translation key """
@@ -62,11 +61,11 @@ class translation_test(unittest.TestCase):
 
     def test_tranlation(self):
         url = 'translation_keys/8ad5a7fe0a12729764e31a1e3ca80059/translations'
-        self.client.read(url)
+        self.client.read(url, {'locale':'ru'})
         key = Key(label = '{actor} give you {count} apples',
                   language = self.lang)
         t = Translation.from_data(key,
-                                  self.client.get(url, {})['results'])
+                                  self.client.get(url, {'locale':'ru'})['results'])
         self.assertEquals('Маша любезно дала тебе 2 яблока', t.execute({'actor':Gender.female('Маша'),'count':2}, {}), 'Female few')
         self.assertEquals('Вася дал тебе всего 1 яблоко, мужик!', t.execute({'actor':{'gender':'male','name':'Вася'},'count':1}, {}), 'Male one')
         self.assertEquals('Вася дал тебе 5 яблок', t.execute({'actor':{'gender':'male','name':'Вася'},'count':5}, {}), 'Male many')

bec48080760606b180ea996ae4fe6ab79756d280
Andrey Toukmanov <a@toukmanov.ru>
2015-03-06 22:40:03 +0800

    demo server

--- demo/app.py
+++ demo/app.py
@@ -1,8 +1,11 @@
 # encoding: UTF-8
 from tml import configure, tr, Error
-from flask import Flask, render_template, request
-from json import loads
+from flask import Flask, render_template, request, make_response
+from json import loads, dumps
+import re
+DEBUG = True
 
+IS_JSON = re.compile('application/json')
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,14 +19,15 @@ def translate():
 
     for key in ['token', 'locale', 'label', 'description', 'data']:
         data[key] = request.values.get(key)
-    handle = None
-    try:
-        data['args'] = loads(data['data'].encode('utf-8')) if len(data['data']) else {}
-        configure(token = data['token'], locale = data['locale'])
-        data['result'] = tr(data['label'], data['args'], data['description'])
-    except handle as e:
-        data['error_class'] = e.__class__.__name__
-        data['error'] = str(e)
+
+    data['args'] = loads(data['data'].encode('utf-8')) if len(data['data']) else {}
+    configure(token = data['token'], locale = data['locale'])
+    data['result'] = tr(data['label'], data['args'], data['description'])
+
+    if IS_JSON.match(request.headers['Accept']):
+        if 'result' in data:
+            return make_response(dumps({'result': data['result']}))
+
     return render_template('index.html', **data)
 
 if __name__ == '__main__':

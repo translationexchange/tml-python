01590a5301ba97eaee32daf34df2916d499096dd
Andrey Toukmanov <a@toukmanov.ru>
2015-03-19 15:23:22 +0700

    Custom env for django

--- tml_django_demo/manage.py
+++ tml_django_demo/manage.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+try:
+    import env
+except ImportError:
+    pass
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tml_django_demo.settings")

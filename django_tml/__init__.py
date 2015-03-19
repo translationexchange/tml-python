6ce30306cf1cc75f2bdab781527fd2e3fcb64dbb
Andrey Toukmanov <a@toukmanov.ru>
2015-03-20 04:14:42 +0700

    activate locale

--- django_tml/__init__.py
+++ django_tml/__init__.py
@@ -6,6 +6,9 @@ from django.conf import settings
 def tr(label, data = None, description = None, options = {}):
     return Translator.instance().context.tr(label, data, description, options)
 
+def activate(locale):
+    Translator.instance().activate(locale)
+
 if settings.TML.get('monkeypatch', False):
     translation._trans = Translator.instance()
     _supported = [(lang['locale'], lang['native_name']) for lang in translation._trans.default.application.languages]

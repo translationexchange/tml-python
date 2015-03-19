828352c5332a05a94d7fe301a612defdec011351
Andrey Toukmanov <a@toukmanov.ru>
2015-03-19 15:24:31 +0700

    Use django cache

---
+++ django_tml/cache.py
@@ -0,0 +1,42 @@
+# encoding: UTF-8
+from django.conf import settings
+from django.core.cache import get_cache
+from urllib import urlencode
+from tml.api.client import Client
+
+
+class CachedClient(object):
+    def __init__(self, client, backend):
+        self.client = client
+        self.backend = backend
+
+    @classmethod
+    def instance(cls):
+        client = Client(settings.TML.get('token'))
+        backend_name = settings.TML.get('cache', None)
+        if backend_name is None:
+            return client
+        return cls(client, get_cache(backend_name))
+
+    def get(self, url, params = {}):
+        """ GET request to API 
+            Args:
+                url (string): URL
+                params (dict): params
+            Raises:
+                HttpError: something wrong with connection
+                APIError: API returns error
+            Returns:
+                dict: response
+        """
+        key = '%s?%s' % (url, urlencode(params))
+        ret = self.backend.get(key)
+        if not ret is None:
+            return ret
+        ret = self.client.get(url, params)
+        self.backend.set(key, ret)
+        return ret
+
+    def post(self, url, params):
+        self.client.post(url, params)
+

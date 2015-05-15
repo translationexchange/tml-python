ca6ceef08dac50d68107a77b1f757827da1ee43f
Andrey Toukmanov <a@toukmanov.ru>
2015-05-15 14:59:27 +0300

    Fix pagination unittest encoding

--- tests/unit/api/pagination.py
+++ tests/unit/api/pagination.py
@@ -4,6 +4,7 @@ from tests.mock import Client
 from tml.api.pagination import allpages
 import unittest
 from six.moves import range
+from tml.strings import to_string
 
 class PaginationTest(unittest.TestCase):
     def setUp(self):
@@ -22,8 +23,8 @@ class PaginationTest(unittest.TestCase):
             self.client.read('applications/2/translations', {'locale':'ru', 'page':page}, 'applications/2/translations_%d.json' % page)
         full_list = allpages(self.client, 'applications/2/translations', {'locale':'ru'})
         self.assertEquals(5, len(full_list), 'Test paging')
-        self.assertEquals("{actor} любезно дала тебе {count||яблоко, яблока, яблок}", full_list['8ad5a7fe0a12729764e31a1e3ca80059'][0]['label'], 'Check first')
-        self.assertEquals("Тест", full_list['5174f88691edb354a9f46af6e7455bb8'][0]['label'], 'Check last')
+        self.assertEquals(to_string("{actor} любезно дала тебе {count||яблоко, яблока, яблок}"), full_list['8ad5a7fe0a12729764e31a1e3ca80059'][0]['label'], 'Check first')
+        self.assertEquals(to_string("Тест"), full_list['5174f88691edb354a9f46af6e7455bb8'][0]['label'], 'Check last')
 
 
     def test_no_pagination(self):

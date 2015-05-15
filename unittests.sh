097a89c7f4229eb884af74bea46a54ecddef233d
Andrey Cloud Toukmanov <a@toukmanov.ru>
2015-05-15 16:28:38 +0000

    Run all unitests

--- unittests.sh
+++ unittests.sh
@@ -2,5 +2,5 @@
 PATH_TO_SCRIPT=$(cd ${0%/*} && echo $PWD/${0##*/})
 PATH_TO_FOLDER=`dirname "$PATH_TO_SCRIPT"`
 export PYTHONPATH=$PYTHONPATH:$PATH_TO_FOLDER
-cd $PATH_TO_FOLDER; nosetests tests/unit/*
+cd $PATH_TO_FOLDER; nosetests tests/unit/* --all --verbose
 
